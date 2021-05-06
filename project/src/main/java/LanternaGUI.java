c8dfa7841a32a7760d1d295b869f684aa99b4485
Vasco Gomes <up201906617@fe.up.pt>
2021-05-06 10:33:20 +0100

    GUI - Not used yet in Game nor in ArenaDrawer

---
+++ project/src/main/java/LanternaGUI.java
@@ -0,0 +1,75 @@
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.graphics.TextGraphics;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
+
+public class LanternaGUI implements GUI{
+    private final Screen screen;
+
+    public LanternaGUI(int width, int height) throws IOException {
+        Terminal terminal = createTerminal(width,height);
+        this.screen = createScreen(terminal);
+    }
+
+    private Screen createScreen(Terminal terminal) throws IOException {
+        Screen screen = new TerminalScreen(terminal);
+        screen.setCursorPosition(null);
+        screen.startScreen();
+        screen.doResizeIfNecessary();
+        return screen;
+    }
+
+    private Terminal createTerminal(int width, int height) throws IOException {
+        TerminalSize terminalSize = new TerminalSize(width,height+1);
+        DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
+        Terminal terminal = terminalFactory.createTerminal();
+        return terminal;
+    }
+
+    @Override
+    public void clear() {
+        screen.clear();
+    }
+
+    @Override
+    public void refresh() throws IOException {
+        screen.refresh();;
+    }
+
+    @Override
+    public void close() throws IOException {
+        screen.close();
+    }
+
+    @Override
+    public MOVEMENT getNextMovement() throws IOException {
+        KeyStroke keyStroke = screen.readInput();
+        if(keyStroke.getKeyType() == KeyType.EOF)
+            return MOVEMENT.QUIT;
+        if(keyStroke.getKeyType()!= KeyType.Character)
+            return MOVEMENT.NONE;
+        if(keyStroke.getCharacter()=='q')
+            return MOVEMENT.QUIT;
+        if(keyStroke.getCharacter()=='w')
+            return MOVEMENT.UP;
+        if(keyStroke.getCharacter()=='s')
+            return MOVEMENT.DOWN;
+        if(keyStroke.getCharacter()=='e')
+            return MOVEMENT.DOUBLEUP;
+        return MOVEMENT.NONE;
+    }
+
+    @Override
+    public void drawText(Position position, String text, String color) {
+        TextGraphics textGraphics = screen.newTextGraphics();
+        textGraphics.setBackgroundColor(TextColor.Factory.fromString(color));
+        textGraphics.putString(position.getX(),position.getY(),text);
+    }
+}
